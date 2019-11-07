1dff773b5f61e614a48b17eddae776c59f6740a6
Bram Duvigneau <bram@bramd.nl>
2019-11-07 11:38:19 +0100

    Fix dependencies

--- addon/globalPlugins/sentry_globalPlugin/__init__.py
+++ addon/globalPlugins/sentry_globalPlugin/__init__.py
@@ -1,10 +1,13 @@
+import os, sys
 import addonHandler
 addonHandler.initTranslation()
 import globalPluginHandler
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+import sentry_sdk
+sys.path.remove(sys.path[-1])
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     def __init__(self):
         super(GlobalPlugin, self).__init__()
-        import sentry_sdk
         sentry_sdk.init("https://1475ccc206a946a68eb48e27dab51825@sentry.io/1782127")
